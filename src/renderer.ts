94475fd94a260fed8ba3be4f9d68bfc84a5b1766
David Epely <esion99@gmail.com>
2022-11-22 12:40:08 +0100

    display fret number on notes

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,14 +4,15 @@
  * To Public License, Version 2, as published by Sam Hocevar. See
  * http://www.wtfpl.net/ for more details. */
 
-import { Application, Graphics, ICanvas } from 'pixi.js';
+import { Application, Graphics, ICanvas, Text, TextStyle } from 'pixi.js';
 
 const SCREEN_HEIGHT = 300;
 const SCREEN_WIDTH = 600;
 const GUITAR_HEIGHT = 200;
 const MARGIN_Y = (SCREEN_HEIGHT - GUITAR_HEIGHT) / 2;
 const STRING_HEIGHT = 3;
-const NOTE_HEIGHT = 10;
+const NOTE_HEIGHT = 13;
+const NOTE_RADIUS = 4;
 
 const posYGstrings = {
   ETreble: MARGIN_Y + GUITAR_HEIGHT / 6,
@@ -26,7 +27,15 @@ type IGString = keyof typeof posYGstrings;
 
 class Note {
   // XXX: should compute itself string & fret if no indications
-  constructor({ gString, x }: { gString: IGString; x: number }) {
+  constructor({
+    gString,
+    x,
+    fretNumber,
+  }: {
+    gString: IGString;
+    x: number;
+    fretNumber: number;
+  }) {
     this.background = new Graphics();
     this.background.beginFill(0x33c7de);
     this.background.drawRoundedRect(
@@ -34,21 +43,29 @@ class Note {
       0,
       NOTE_HEIGHT * 2,
       NOTE_HEIGHT,
-      NOTE_HEIGHT
+      NOTE_RADIUS
     );
     this.background.endFill();
     // tmp
     this.background.x = x;
 
+    const style = new TextStyle({
+      fontSize: 10,
+    });
+    this.fret = new Text(fretNumber.toString(), style);
+    this.fret.x = x + 3;
+
     this.gString = gString;
   }
 
   background: Graphics;
+  fret: Text;
   _gString: IGString = 'EBass';
 
   set gString(gString: IGString) {
     this._gString = gString;
     this.background.y = posYGstrings[gString] - NOTE_HEIGHT / 2;
+    this.fret.y = posYGstrings[gString] - NOTE_HEIGHT / 2;
   }
 
   get gString() {
@@ -58,6 +75,7 @@ class Note {
 
   render(app: Application<ICanvas>) {
     app.stage.addChild(this.background);
+    app.stage.addChild(this.fret);
   }
 }
 
@@ -90,11 +108,19 @@ export const setupRenderer = ({ contentEl }: { contentEl: HTMLElement }) => {
   app.stage.addChild(center);
 
   // --- NOTES
-  const note1 = new Note({ gString: 'A', x: SCREEN_WIDTH - 50 });
+  const note1 = new Note({ gString: 'A', x: SCREEN_WIDTH - 50, fretNumber: 3 });
   note1.render(app);
-  const note2 = new Note({ gString: 'A', x: SCREEN_WIDTH - 100 });
+  const note2 = new Note({
+    gString: 'A',
+    x: SCREEN_WIDTH - 100,
+    fretNumber: 5,
+  });
   note2.render(app);
-  const note3 = new Note({ gString: 'EBass', x: SCREEN_WIDTH - 150 });
+  const note3 = new Note({
+    gString: 'EBass',
+    x: SCREEN_WIDTH - 150,
+    fretNumber: 0,
+  });
   note3.render(app);
   // ---
 
